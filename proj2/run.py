be3f90447e505816fb6286e0f8071b89bdc07ef9
hyemmie <lhm0710@naver.com>
2022-10-24 23:31:36 +0900

    impl create normal column

--- proj2/run.py
+++ proj2/run.py
@@ -1,19 +1,103 @@
 from lark import Lark
-import sys
+from berkeleydb import db
+import sys, os
+from messages import Message
+from transformer import *
 
 PROMPT_NAME = 'DB_2017-19651> '
 ERROR_STRING = 'Syntax error'
+    
+# def create_table(table_name):
+#   b_db = db.DB()
+#   b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH, flags=db.DB_CREATE)
+#   b_db.put(b'apple', b"red")
+#   print(b_db.get(b"apple"))
+#   b_db.close()
+
+
+# # TODO: drop reference error
+# def drop_table(table_name):
+#   db_path = "./db/{}.db".format(table_name)
+#   try:
+#     os.remove(db_path)
+#     print(Message.DropSuccess(table_name))
+#   except:
+#       print(Message.NoSuchTable)
+
+
+# def desc_table(table_name):
+#   print("-------------------------------------------------")
+#   print("table_name [{}]".format(table_name))
+#   print("column_name        type      null      key")
+#   # for 
+#   print("-------------------------------------------------")
+
+
+# def show_tables():
+#   print('----------------')
+#   path = "./db/"
+#   file_list = os.listdir(path)
+#   table_list = [file for file in file_list if file.endswith(".db")]
+#   for table in table_list:
+#     print(table.split(".")[0])
+#   print('----------------')
+
+# myDB = db.DB()
+# myDB.open('./db/myDB3.db', dbtype=db.DB_HASH, flags=db.DB_CREATE)
+# myDB.put(b'apple', b"red")
+# print(None == myDB.get(b"red"))
+# myDB.close()
+# create_table("test")
+# show_tables()
+#drop_table("myDB")
+# table_name = "desc"
+# b_db = db.DB()
+# b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH, flags=db.DB_CREATE)
+# b_db.put(b"column_num", b"4")
+
+# b_db.put(b"column_1_name", b"account")
+# b_db.put(b"column_1_type", b"int")
+# b_db.put(b"column_1_nullable", b"N")
+# b_db.put(b"column_1_is_primary_key", b"PRI")
+# b_db.put(b"column_1_reference_table", b"another")
+
+# b_db.put(b"column_2_name", b"branch")
+# b_db.put(b"column_2_type", b"char(15)")
+# b_db.put(b"column_2_nullable", b"Y")
+
+# b_db.put(b"column_3_name", b"id")
+# b_db.put(b"column_3_type", b"int")
+# b_db.put(b"column_3_nullable", b"N")
+# b_db.put(b"column_3_reference_table", b"other")
+
+# b_db.put(b"column_4_name", b"detail")
+# b_db.put(b"column_4_type", b"char(15)")
+# b_db.put(b"column_4_nullable", b"N")
+# b_db.put(b"column_4_is_primary_key", b"PRI")
+# b_db.close()
+
+# QueryTransformer.drop_table_query()
+
+# table_name = "account"
+# b_db = db.DB()
+# b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH, flags=db.DB_CREATE)
+# cursor = b_db.cursor()
+# while x := cursor.next():
+#   print(x)
 
 # printing method
-def parse_result(query): 
+def parse_result(query, result): 
   # remove blank after semicolon
   query = query.lstrip()
   if query[:12] == 'create table':
-    print(PROMPT_NAME + "'CREATE TABLE' requested")
+    QueryTransformer().transform(result)
+    # print(PROMPT_NAME + "'CREATE TABLE' requested")
   elif query[:10] == 'drop table':
-    print(PROMPT_NAME + "'DROP TABLE' requested")
+    QueryTransformer().transform(result)
+#    print(PROMPT_NAME + "'DROP TABLE' requested")
   elif query[:4] == 'desc':
-    print(PROMPT_NAME + "'DESC' requested")
+    QueryTransformer().transform(result)
+  #  print(PROMPT_NAME + "'DESC' requested")
   elif query[:6] == 'insert':
     print(PROMPT_NAME + "'INSERT' requested")
   elif query[:6] == 'delete':
@@ -21,13 +105,13 @@ def parse_result(query):
   elif query[:6] == 'select':
     print(PROMPT_NAME + "'SELECT' requested")
   elif query[:4]== 'show':
-    print(PROMPT_NAME + "'SHOW TABLES' requested")
+#    print(PROMPT_NAME + "'SHOW TABLES' requested")
+    QueryTransformer().transform(result)
   elif query[:6] == 'update':
     print(PROMPT_NAME + "'UPDATE' requested")
   elif query[:4] == 'exit':
     raise SystemExit
 
-# main routine
 while True:
   with open('grammar.lark') as file:
     sql_parser = Lark(file.read(), start="command", lexer="basic")
@@ -44,10 +128,11 @@ while True:
       query_with_semicolon = query + ";"
       try: 
         result = sql_parser.parse(query_with_semicolon)
-        parse_result(query_with_semicolon)
+        parse_result(query_with_semicolon, result)
       except SystemExit:
         sys.exit(0)
-      except:
+      except(error):
+        print(error) 
         print(PROMPT_NAME + ERROR_STRING)
         break;
 
@@ -70,10 +155,11 @@ while True:
       query_with_semicolon = query_list.pop(0) + ";"
       try:
         result = sql_parser.parse(query_with_semicolon)
-        parse_result(query_with_semicolon)
+        parse_result(query_with_semicolon, result)
       except SystemExit:
         sys.exit(0)
-      except:
+      except(error):
+        print(error)
         print(PROMPT_NAME + ERROR_STRING)
         break;
       
--- proj2/transformer.py
+++ proj2/transformer.py
@@ -1,4 +1,5 @@
-from lark import Lark, Transformer
+from lark import Lark, Transformer, Tree
+from lark.visitors import CollapseAmbiguities
 from messages import Message
 import os, sys
 from berkeleydb import db
@@ -7,40 +8,14 @@ PROMPT_NAME = 'DB_2017-19651> '
 ERROR_STRING = 'Syntax error'
 
 class QueryTransformer(Transformer):
-  # printing method
-  def parse_result(query): 
-    # remove blank after semicolon
-    query = query.lstrip()
-    if query[:12] == 'create table':
-      print(PROMPT_NAME + "'CREATE TABLE' requested")
-    elif query[:10] == 'drop table':
-      drop_table()
-  #    print(PROMPT_NAME + "'DROP TABLE' requested")
-    elif query[:4] == 'desc':
-      desc_table()   
-  #    print(PROMPT_NAME + "'DESC' requested")
-    elif query[:6] == 'insert':
-      print(PROMPT_NAME + "'INSERT' requested")
-    elif query[:6] == 'delete':
-      print(PROMPT_NAME + "'DELETE' requested")
-    elif query[:6] == 'select':
-      print(PROMPT_NAME + "'SELECT' requested")
-    elif query[:4]== 'show':
-  #    print(PROMPT_NAME + "'SHOW TABLES' requested")
-      show_tables()
-    elif query[:6] == 'update':
-      print(PROMPT_NAME + "'UPDATE' requested")
-    elif query[:4] == 'exit':
-      raise SystemExit
+  query_cols = 0
+  column_names = []
+  is_primary_key_set = False
 
-  def transform_result(query, result):
-    print(result)
-    parse_result(query) 
-    print(transform(result))
+  def create_table_query(self, items):
+    table_name = items[2].children[0].value
 
-
-  def create_table_query():
-    table_name = ""
+    # check table_name exists
     path = "./db/"
     file_list = os.listdir(path)
     table_list = [file for file in file_list if file.endswith(".db")]
@@ -48,14 +23,120 @@ class QueryTransformer(Transformer):
       if table_name.lower() == table.split(".")[0].lower():
         return print(Message.TableExistenceError.value)
 
+    # open new db
     b_db = db.DB()
     b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH, flags=db.DB_CREATE)
-    b_db.put(b'apple', b"red")
-    print(b_db.get(b"apple"))
     b_db.close()
 
-  def drop_table_query():
-    table_name = "" 
+    # extract table element list
+    table_element_list = items[3].children
+    if table_element_list[0].value == "(":
+      table_element_list.pop(0)
+    if table_element_list[-1].value == ")":
+      table_element_list.pop(-1)
+    
+    for elem in table_element_list:
+      print(elem.children[0].data)
+      if elem.children[0].data == "column_definition":
+        result = self.create_column_definition(elem, table_name)
+        if result < 0:
+          break
+      if elem.children[0].data == "table_constraint_definition":
+        result = self.create_table_constraint_definition(elem, table_name)
+        if result < 0:
+          break
+    
+    # set final column numbers
+    b_db = db.DB()
+    b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH)
+    b_db.put(b"column_num", bytes(self.query_cols, 'utf-8'))
+    b_db.close()
+  
+  
+  def create_column_definition(self, tree, table_name):
+    self.query_cols += 1
+    print(self.query_cols)
+    # check DuplicateColumnDefError
+    column_name = tree.children[0].children[0].children[0].value
+    print(column_name)
+    for name in self.column_names:
+      if name.lower() == column_name.lower():
+        print(Message.DuplicateColumnDefError.value)
+        return -1
+    # check CharLengthError
+    data_type_list = tree.children[0].children[1].children
+    if data_type_list[0].value == "char":
+      if int(data_type_list[2].value) < 1:
+        print(Message.CharLengthError.value)
+        return -1
+    data_type_str = ""
+    for data in data_type_list:
+      data_type_str += data.value
+    print(data_type_str)
+    
+    is_nullable = "Y"
+    if len(tree.children[0].children) > 2:
+      if tree.children[0].children[2] == "not" and tree.children[0].children[3] == "null":
+        is_nullable = "N"
+    print(is_nullable)
+
+    # save on DB
+    b_db = db.DB()
+    b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH)
+    b_db.put(bytes("column_{}_name".format(self.query_cols), 'utf-8'), bytes(column_name, 'utf-8'))
+    b_db.put(bytes("column_{}_type".format(self.query_cols), 'utf-8'), bytes(data_type_str, 'utf-8'))
+    b_db.put(bytes("column_{}_null".format(self.query_cols), 'utf-8'), bytes(is_nullable, 'utf-8'))
+    b_db.close()
+    self.column_names.append(column_name)
+    return 1
+
+
+  def create_table_constraint_definition(self, tree, table_name):
+    if tree.children[0].children[0].data.value == "referential_constraint":
+      print(tree.pretty())
+      column_name_list = tree.children[0].children[0].children[2].children
+      print(column_name_list)
+      # reference_name
+     
+      print(tree.children[0].children[0].data.value)
+    if tree.children[0].children[0].data.value == "primary_key_constraint":
+      if self.is_primary_key_set == True:
+        print(Message.DuplicatePrimaryKeyDefError.value)
+        return -1
+      self.is_primary_key_set = True
+      # get primary key list
+      primary_key_list = tree.children[0].children[0].children[2].children
+      if primary_key_list[0].value == "(":
+        primary_key_list.pop(0)
+      if primary_key_list[-1].value == ")":
+        primary_key_list.pop(-1)
+      # form into one string to save at DB
+      primary_key_string = ""
+      for key_tree in primary_key_list:
+        key_name = key_tree.children[0].value
+        print(key_name)
+        if key_name in self.column_names:
+          primary_key_string += key_name + ','
+          # set is_primary_key to each columns
+          index = self.column_names.index(key_name) + 1
+          b_db = db.DB()
+          b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH)
+          b_db.put(bytes("column_{}_is_primary_key".format(index), 'utf-8'), b"Y")
+          b_db.put(bytes("column_{}_is_null".format(index), 'utf-8'), b"N")
+          b_db.close()
+        else:
+          print(Message.NonExistingColumnDefError(key_name))
+          return -1
+      primary_key_string = primary_key_string[:-1]
+      print(primary_key_string)
+      b_db = db.DB()
+      b_db.open('./db/{}.db'.format(table_name), dbtype=db.DB_HASH)
+      b_db.put(b"primary_keys", bytes(primary_key_string, 'utf-8'))
+      b_db.close()
+    return 1
+      
+  def drop_table_query(self, items):
+    table_name = items[2].children[0].value
     path = "./db/"
     file_list = os.listdir(path)
     table_list = [file for file in file_list if file.endswith(".db")]
@@ -65,23 +146,22 @@ class QueryTransformer(Transformer):
         b_db.close()
         continue
       else:
-        print(table.split(".")[0])
         b_db.open('./db/{}.db'.format(table.split(".")[0]), dbtype=db.DB_HASH)
         num_cols = b_db.get(b"column_num")
-        for i in range(1, num_cols + 1):
-          reference_table = b_db.get(b"column_{}_reference_table".format(i))
-          if reference_table.lower() == table_name.lower():
-            b_db.close()
-            return print(Message.DropReferencedTableError(table_name))
+        # for i in range(1, num_cols + 1):
+        #   reference_table = b_db.get(b"column_{}_reference_table".format(i))
+        #   if reference_table.lower() == table_name.lower():
+        #     b_db.close()
+        #     return print(Message.DropReferencedTableError(table_name))
         b_db.close()
     try:
       db_path = "./db/{}.db".format(table_name)
       os.remove(db_path)
-      print(Message.DropSuccess(table_name))
+      print(PROMPT_NAME + Message.DropSuccess(table_name))
     except:
       print(Message.NoSuchTable.value)
 
-  def desc_table_query():
+  def desc_table_query(self, items):
     table_name = ""
     b_db = db.DB()
     try:
