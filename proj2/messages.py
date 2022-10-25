fe1b285d2b78e4f897771ff64fba392566413f95
hyemmie <lhm0710@naver.com>
2022-10-25 23:43:37 +0900

    remove comments

--- proj2/messages.py
+++ proj2/messages.py
@@ -1,15 +1,14 @@
 from enum import Enum
 
 class Message(Enum):
+
   DuplicateColumnDefError = "Create table has failed: column definition is duplicated"
   DuplicatePrimaryKeyDefError = "Create table has failed: primary key definition is duplicated"
   ReferenceTypeError = "Create table has failed: foreign key references wrong type"
   ReferenceNonPrimaryKeyError = "Create table has failed: foreign key references non primary key column" 
   ReferenceColumnExistenceError = "Create table has failed: foreign key references non existing column"
   ReferenceTableExistenceError = "Create table has failed: foreign key references non existing table"
-  # check
   TableExistenceError = "Create table has failed: table with the same name already exists"
-  # check 2/2
   NoSuchTable = "No such table"
   CharLengthError = "Char length should be over 0"
 
@@ -19,10 +18,9 @@ class Message(Enum):
   def NonExistingColumnDefError(col_name):
     return "Create table has failed: '{}' does not exists in column definition".format(col_name)
 
-  # check
+
   def DropSuccess(table_name):
     return "'{}' table is dropped".format(table_name)
 
-  # check without test
   def DropReferencedTableError(table_name):
     return "Drop table has failed: '{}' is referenced by other table".format(table_name)
