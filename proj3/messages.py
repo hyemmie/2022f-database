714e71a163385cda6bec93d7ca36f11e2e636047
hyemmie <lhm0710@naver.com>
2022-10-29 23:54:52 +0900

    Init proj3

---
+++ proj3/messages.py
@@ -0,0 +1,26 @@
+from enum import Enum
+
+class Message(Enum):
+
+  DuplicateColumnDefError = "Create table has failed: column definition is duplicated"
+  DuplicatePrimaryKeyDefError = "Create table has failed: primary key definition is duplicated"
+  ReferenceTypeError = "Create table has failed: foreign key references wrong type"
+  ReferenceNonPrimaryKeyError = "Create table has failed: foreign key references non primary key column" 
+  ReferenceColumnExistenceError = "Create table has failed: foreign key references non existing column"
+  ReferenceTableExistenceError = "Create table has failed: foreign key references non existing table"
+  TableExistenceError = "Create table has failed: table with the same name already exists"
+  NoSuchTable = "No such table"
+  CharLengthError = "Char length should be over 0"
+
+  def CreateTableSuccess(table_name):
+    return "'{}' table is created".format(table_name)
+
+  def NonExistingColumnDefError(col_name):
+    return "Create table has failed: '{}' does not exists in column definition".format(col_name)
+
+
+  def DropSuccess(table_name):
+    return "'{}' table is dropped".format(table_name)
+
+  def DropReferencedTableError(table_name):
+    return "Drop table has failed: '{}' is referenced by other table".format(table_name)
---
+++ proj3/run.py
@@ -0,0 +1,82 @@
+from lark import Lark
+from berkeleydb import db
+import sys, os
+from messages import Message
+from transformer import *
+
+PROMPT_NAME = 'DB_2017-19651> '
+ERROR_STRING = 'Syntax error'
+
+# printing method
+def parse_result(query, result): 
+  # remove blank after semicolon
+  query = query.lstrip()
+  if query[:12].lower() == 'create table':
+    QueryTransformer().transform(result)
+  elif query[:10].lower() == 'drop table':
+    QueryTransformer().transform(result)
+  elif query[:4].lower() == 'desc':
+    print(result)
+    QueryTransformer().transform(result)
+  elif query[:6].lower() == 'insert':
+    print(PROMPT_NAME + "'INSERT' requested")
+  elif query[:6].lower() == 'delete':
+    print(PROMPT_NAME + "'DELETE' requested")
+  elif query[:6].lower() == 'select':
+    print(PROMPT_NAME + "'SELECT' requested")
+  elif query[:4].lower() == 'show':
+    QueryTransformer().transform(result)
+  elif query[:6].lower() == 'update':
+    print(PROMPT_NAME + "'UPDATE' requested")
+  elif query[:4].lower() == 'exit':
+    raise SystemExit
+
+while True:
+  with open('grammar.lark') as file:
+    sql_parser = Lark(file.read(), start="command", lexer="basic")
+  query_str = input(PROMPT_NAME)
+  query_list = query_str.split(';')
+
+  # CASE 1: query finished with semicolon in one line
+  if (query_list[-1] == ""):
+    # pop last blank by split
+    query_list.pop()
+
+    while len(query_list) > 0:
+      query = query_list.pop(0)
+      query_with_semicolon = query + ";"
+      try: 
+        result = sql_parser.parse(query_with_semicolon)
+        parse_result(query_with_semicolon, result)
+      except SystemExit:
+        sys.exit(0)
+      except:
+        print(PROMPT_NAME + ERROR_STRING)
+        break;
+
+  # CASE 2: query didn't finish with semicolon in one line
+  # multi-line query/queries
+  else:
+    while query_list[-1] != "":
+      query_list[-1] += " "
+      new_query_str = input()
+      new_query_list = new_query_str.split(';')
+      # continue unfinished last query
+      if len(new_query_list) > 0:
+        query_list[-1] += new_query_list.pop(0)
+      query_list = query_list + new_query_list
+
+    # pop last blank by split
+    query_list.pop()
+
+    while len(query_list) > 0:
+      query_with_semicolon = query_list.pop(0) + ";"
+      try:
+        result = sql_parser.parse(query_with_semicolon)
+        parse_result(query_with_semicolon, result)
+      except SystemExit:
+        sys.exit(0)
+      except:
+        print(PROMPT_NAME + ERROR_STRING)
+        break;
+      
