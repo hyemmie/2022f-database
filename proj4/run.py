47b3917cb90ff621a55d3a06819be2c348e5578b
hyemmie <lhm0710@naver.com>
2022-12-12 23:56:27 +0900

    feat movie and audience

--- proj4/run.py
+++ proj4/run.py
@@ -1,17 +1,169 @@
+import pymysql.cursors
+import csv
 
+STUDENT_ID = 'DB2017_19651'
+TABLE_MOVIE = 'Movie'
+TABLE_AUDIENCE = 'Audience'
+TABLE_BOOK = 'Book'
+TABLE_RATE = 'Rate'
+
+MALE = 'male'
+FEMALE = 'female'
+
+def connect_db():
+    return pymysql.connect(
+        host='astronaut.snu.ac.kr',
+        port=7000, 
+        user=STUDENT_ID, 
+        password=STUDENT_ID, 
+        database=STUDENT_ID,
+    )
 
 # Problem 1 (5 pt.)
 def reset():
     # YOUR CODE GOES HERE
+    drop_table(TABLE_RATE)
+    drop_table(TABLE_BOOK)
+    drop_table(TABLE_AUDIENCE)
+    drop_table(TABLE_MOVIE)
+    create_table(TABLE_MOVIE)
+    create_table(TABLE_AUDIENCE)
+    create_table(TABLE_BOOK)
+    create_table(TABLE_RATE)
 
+    initialize()
     print('Initialized database')
     # YOUR CODE GOES HERE
     pass
 
+def drop_table(table_name):
+    with connection.cursor() as cursor:
+        sql = f"DROP TABLE IF EXISTS {table_name}"
+        result = cursor.execute(sql)
+    connection.commit()
+
+def create_table(table_name):
+    if table_name == TABLE_MOVIE:
+        with connection.cursor() as cursor:
+            sql= f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                    `m_id` INT NOT NULL AUTO_INCREMENT,
+                    `title` VARCHAR(80) NOT NULL,
+                    `director` VARCHAR(80) NOT NULL,
+                    `price` INT UNSIGNED NOT NULL,
+                    PRIMARY KEY (`m_id`))"""
+            cursor.execute(sql)
+    if table_name == TABLE_AUDIENCE:
+        with connection.cursor() as cursor:
+            sql= f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                    `a_id` INT NOT NULL AUTO_INCREMENT,
+                    `name` VARCHAR(80) NOT NULL,
+                    `gender` ENUM('{MALE}', '{FEMALE}') NOT NULL,
+                    `age` INT UNSIGNED NOT NULL,
+                    PRIMARY KEY (`a_id`))"""
+            cursor.execute(sql)
+    if table_name == TABLE_BOOK:
+        with connection.cursor() as cursor:
+            sql= f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                    `b_id` INT NOT NULL AUTO_INCREMENT,
+                    `m_id` INT NOT NULL,
+                    `a_id` INT NOT NULL,
+                    PRIMARY KEY (`b_id`),
+                    CONSTRAINT `book_table`
+                        FOREIGN KEY (`m_id`)
+                        REFERENCES {TABLE_MOVIE} (`m_id`)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE,
+                    CONSTRAINT `book_audience`
+                        FOREIGN KEY (`a_id`)
+                        REFERENCES {TABLE_AUDIENCE} (`a_id`)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE)"""
+            cursor.execute(sql)
+    if table_name == TABLE_RATE:
+        with connection.cursor() as cursor:
+            sql= f"""CREATE TABLE IF NOT EXISTS {table_name} (
+                    `r_id` INT NOT NULL AUTO_INCREMENT,
+                    `b_id` INT NOT NULL,
+                    `score` INT NOT NULL,
+                    PRIMARY KEY (`r_id`),
+                    CONSTRAINT `rate_book`
+                        FOREIGN KEY (`b_id`)
+                        REFERENCES {TABLE_BOOK} (`b_id`)
+                        ON DELETE CASCADE
+                        ON UPDATE CASCADE)"""
+            cursor.execute(sql)
+    connection.commit()
+
+def initialize():
+    f = open('data.csv','r')
+    rdr = csv.reader(f)
+    is_first = True
+ 
+    for line in rdr:
+        if is_first:
+            is_first = False
+        else:
+            insert_movie_inner(line[0], line[1], line[2])
+            insert_audience_inner(line[3], line[4], line[5])
+
+    f.close()
+
+def insert_movie_inner(title, director, price):
+    with connection.cursor() as cursor:
+        sql= f"""INSERT INTO {TABLE_MOVIE}
+                (`title`, `director`, `price`)
+                VALUES (%s, %s, %s)"""
+        cursor.execute(sql, (title, director, price))
+    connection.commit()
+
+def insert_audience_inner(name, gender, age):
+    with connection.cursor() as cursor:
+        sql= f"""INSERT INTO {TABLE_AUDIENCE}
+                (`name`, `gender`, `age`)
+                VALUES (%s, %s, %s)"""
+        cursor.execute(sql, (name, gender, age))
+    connection.commit()
+
+def print_line(data, lengthList):
+    data = list(map(str, data))
+    length = len(data)
+    formatStr = ""
+    for i in range(0, length):
+        formatStr = formatStr + "{" + "{num}:<{len}s".format(num=i, len=lengthList[i]) + "}"
+        if i != length-1:
+            formatStr = formatStr + "    "
+    print(formatStr.format(*data))
+    
+def print_align(column_list, records):
+    col_num = len(column_list)
+    maxLen = [0] * col_num
+    
+    for i in range(0, colNum):
+        maxLen[i] = len(titleList[i])
+
+    for rec in records:
+        for i in range(0, colNum):
+            maxLen[i] = max(maxLen[i], len(str(rec[i])))
+    
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    print_line(titleList, maxLen)
+    print("-------------------------------------------------------------------------------------------------------------------------")
+    
+    for i in records:
+        print_line(i, maxLen)
+
 # Problem 2 (3 pt.)
 def print_movies():
     # YOUR CODE GOES HERE
-
+    with connection.cursor() as cursor:
+        sql= f"""SELECT m_id, title, director, price, COUNT(b_id), AVG(score)
+                FROM {TABLE_MOVIE} NATURAL LEFT OUTER JOIN ({TABLE_BOOK} NATURAL JOIN {TABLE_RATE})
+                GROUP BY m_id
+                ORDER BY m_id"""
+        cursor.execute(sql)
+        result = cursor.fetchall()
+    titleList = ('id', 'title', 'director', 'price', 'book count', 'average rate')
+    print_align(titleList, result)
     
     # YOUR CODE GOES HERE
     pass
@@ -205,6 +357,9 @@ def main():
         else:
             print('Invalid action')
 
+    connection.close()
 
+
+connection = connect_db()
 if __name__ == "__main__":
     main()
